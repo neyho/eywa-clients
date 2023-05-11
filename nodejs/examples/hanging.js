353b368ef7d29a7f398fbc71a4c618e8440d4f99
Robert Geršak <robi@neyho.com>
2023-05-11 21:46:26 +0200

    Adding console out to hanging example... To test if canceling processing task will show output as well

--- nodejs/examples/hanging.js
+++ nodejs/examples/hanging.js
@@ -4,6 +4,7 @@ import eywa from 'eywa-reacher-client'
 function execute() {
     eywa.info("start");
     setInterval(() => {
+        console.log('fjqiofjqoijncwioqouhofhqijwifqo');
         eywa.info("repeat");
     }, 10000);
 }
