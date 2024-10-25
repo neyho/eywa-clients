42b57fe51be479a3d7c88f55e228ab6b589e5949
Robert Geršak <robi@neyho.com>
2024-10-25 13:55:18 +0200

    Removing sync version... Leaving only async one and bumping to 0.2.0

--- py/src/eywa/__init__.py
+++ py/src/eywa/__init__.py
@@ -6,6 +6,6 @@ __author__ = "Robert Gersak"
 __email__ = "r.gersak@gmail.com"
 __license__ = "MIT"
 __status__ = "Development"
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 from .eywa_async import *
